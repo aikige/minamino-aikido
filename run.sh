7972e321d033bde334fd2480301d41de158e35ae
aikige <48190566+aikige@users.noreply.github.com>
2026-06-01 18:03:19 +0900

    [up] cleanup.

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+#if [ -d /opt/homebrew/opt/ruby@3.4/bin ]; then
+#	export PATH=/opt/homebrew/opt/ruby@3.4/bin:$PATH
+#fi
 BASEURL='--baseurl='
 DRAFTS=${DRAFTS:-'--drafts'}
 HOST_ADDR=${HOST:-0.0.0.0}
 PORT=${PORT:-4040}
 while [ $# -ge 1 ]; do
 	if [ $1 = "renew" ]; then
-		rm -rf _site
+		rm -rf _site .jekyll-metadata
 	elif [ $1 = "clean" ]; then
 		DRAFTS=
 	elif [ $1 = "subdir" ]; then
@@ -13,4 +16,6 @@ while [ $# -ge 1 ]; do
 	fi
 	shift
 done
+bundle config set --local path 'vendor'
+bundle install || exit $?
 bundle exec jekyll serve $DRAFTS --host=$HOST_ADDR --port=$PORT --incremental $BASEURL
